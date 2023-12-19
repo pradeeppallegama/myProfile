2283e2a42430c7a66bf25bed6745144d1ec2d0d4
pradeeppallegama <40788879+pradeeppallegama@users.noreply.github.com>
2023-12-19 17:12:23 +0530

    chnage service_works

--- service_worker.js
+++ service_worker.js
@@ -2,7 +2,7 @@
 self.addEventListener("install",e=>{
     e.waitUntil(
       caches.open("static").then(cache=>{
-        return cache.addAll(["./",'./images/pradeep-m.png']);
+        return cache.addAll(["./",'./images/logo_new.png']);
       })
     );
   });
